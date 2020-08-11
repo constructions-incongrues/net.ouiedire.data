ec5440dcb3268b5ede235d28331ea8129536d43c
Tristan Rivoallan <tristan@rivoallan.net>
2020-08-11 18:41:55 +0200

    WIP

---
+++ db/collections.sql
@@ -0,0 +1,11 @@
+-- Adminer 4.7.7 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+USE `ouiedire`;
+
+SET NAMES utf8mb4;
+
