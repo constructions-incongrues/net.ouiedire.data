95432e6100b6565f9b218fcb7adc33fa0066eb97
kobischE <ekobsch@gmail.com>
2020-08-24 12:35:59 +0200

    export_02

--- db/sql/current.sql
+++ db/sql/current.sql
@@ -332,7 +332,7 @@ CREATE TABLE `directus_users` (
 
 LOCK TABLES `directus_users` WRITE;
 /*!40000 ALTER TABLE `directus_users` DISABLE KEYS */;
-INSERT INTO `directus_users` VALUES (1,'active',1,'Admin','User','admin@data.ouiedire.net','$2y$10$kMbvH3u4VuU3myXLTrbJM.y3zoG/.65mvEHU6hXTzmDtqn.5vF/Pe','te26GzShbf6kQANI3zMe02B2','UTC','en-US',NULL,NULL,NULL,NULL,1,'2020-08-24 10:21:13','/_/settings/collections',NULL,'auto',NULL,NULL);
+INSERT INTO `directus_users` VALUES (1,'active',1,'Admin','User','admin@data.ouiedire.net','$2y$10$kMbvH3u4VuU3myXLTrbJM.y3zoG/.65mvEHU6hXTzmDtqn.5vF/Pe','te26GzShbf6kQANI3zMe02B2','UTC','en-US',NULL,NULL,NULL,NULL,1,'2020-08-24 10:29:23','/_/settings',NULL,'auto',NULL,NULL);
 /*!40000 ALTER TABLE `directus_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -372,7 +372,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-24 10:23:05
+-- Dump completed on 2020-08-24 10:35:32
 -- MariaDB dump 10.17  Distrib 10.5.4-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: db    Database: ouiedire
@@ -496,7 +496,7 @@ CREATE TABLE `directus_user_sessions` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-24 10:23:06
+-- Dump completed on 2020-08-24 10:35:33
 -- MariaDB dump 10.17  Distrib 10.5.4-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: db    Database: ouiedire
@@ -591,4 +591,4 @@ CREATE TABLE `curators` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-24 10:23:08
+-- Dump completed on 2020-08-24 10:35:35
